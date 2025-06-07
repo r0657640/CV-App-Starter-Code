4b7b17233c24baa871d8077c691423bf7313b63a
r0657640 <125733956+r0657640@users.noreply.github.com>
2025-06-07 23:37:06 +0200

    footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,21 @@
 import HeaderLeft from './components/HeaderLeft';
+import Footer from './components/Footer';
 
 const Layout = ({ children, darkMode, toggleDarkMode }) => {
   return (
+    <div>
     <div className="flex flex-col md:flex-row min-h-screen">
       <div className="w-full md:w-1/3 h-screen sticky top-0 overflow-y-auto border-r border-gray-300 dark:border-gray-700">
         <HeaderLeft darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       </div>
       <div className="w-full md:w-2/3 overflow-y-auto">
         {children}
+       
       </div>
+        
+    
+    </div> 
+    <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// components/Footer.js
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 Dorien Van Loo. Alle rechten voorbehouden.</p>
+       
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
